0ee377bf776e7d91a4d28aa8d9dbfe6cca973426
SWilDev <sean0669@gmail.com>
2021-09-23 18:59:48 +0200

    from linux

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Weather</title>
+
+    </head>
+    <body>
+        <H1>Hello too</H1>
+    </body>
+</html>
