f3fc84cedd2c03ee681528147c2b6ca71a1838d8
SWilDev <sean0669@gmail.com>
2021-10-05 22:13:45 +0200

    flex

--- resources/js/components/Current.vue
+++ resources/js/components/Current.vue
@@ -2,30 +2,35 @@
     <!-- <h1>{{ time }}</h1> -->
     <div class="text-white mb-8">
         <div
-            class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 shadow-lg mt-4"
+            class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 rounded-lg shadow-lg mt-4"
         >
             <div
-                class="current-weather flex items-center justify-between px-6 py-8"
+                class="current-weather flex items-center justify-between px-10 py-8"
             >
-                <div class="flex items-center">
+                <div>
                     <div>
-                        <div class="text-6xl font-semibold">
-                            <p>{{ temperature }} °C</p>
+                        <div class="">
+                            <h1>{{ city }}, {{ country }}</h1>
                         </div>
                         <div>
-                            <p>Ressenti: {{ feels }}°C</p>
+                            <div class="text-6xl font-semibold pt-6 pb-1">
+                                <p>{{ temperature }} °C</p>
+                            </div>
+                            <div class="">
+                                <p>Ressenti: {{ feels }}°C</p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <h1>{{ city }}, {{ country }}</h1>
-                    <h1>Heure: {{ currentTime }}</h1>
-                    <h1>Jour: {{ currentDay }}</h1>
+                    <div class="pb-2">
+                        <h1>{{ currentTime }}, {{ currentDay }}</h1>
+                    </div>
 
                     <h1>Humidite: {{ humidity }} %</h1>
                     <h1>Probabilite de pluie: {{ rainn }} %</h1>
-                    <h1>Coucher de soleil: {{ sunsett }}</h1>
                     <h1>Force du vent: {{ windd }} km/h</h1>
+                    <h1>Coucher de soleil: {{ sunsett }}</h1>
                 </div>
             </div>
         </div>
